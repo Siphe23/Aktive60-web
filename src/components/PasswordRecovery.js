cc8b19f8e3b6e4fd120628c4da1a961bd4f3c28e
Siphe23 <siphesihlemzamane@gmail.com>
2025-02-21 09:15:28 +0200

    Commit

--- src/components/PasswordRecovery.js
+++ src/components/PasswordRecovery.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getFirestore, doc, getDoc } from "firebase/firestore"; // Firestore imports
+import { getFirestore, doc, getDoc } from "firebase/firestore"; 
 import RecoveryImage from "../assets/cuate.png";
 import "../styles/PasswordRecovery.css";
 
 const PasswordRecovery = () => {
-  const [recoveryCode, setRecoveryCode] = useState("");  // To track user input
-  const [error, setError] = useState("");  // For displaying errors if any
+  const [recoveryCode, setRecoveryCode] = useState("");  
+  const [error, setError] = useState("");  
+
   const navigate = useNavigate();
 
   // Firestore setup
@@ -22,20 +23,19 @@ const PasswordRecovery = () => {
     }
 
     try {
-      // Query Firestore to check if the recovery code exists
-      const codeRef = doc(db, "passwordRecoveryCodes", recoveryCode); // Assuming the code is the doc ID
+      
+      const codeRef = doc(db, "passwordRecoveryCodes", recoveryCode); 
       const codeDoc = await getDoc(codeRef);
 
       if (codeDoc.exists()) {
         const codeData = codeDoc.data();
         const currentTime = Date.now();
 
-        // Check if the recovery code has expired (1 hour expiry)
         if (currentTime > codeData.expiration) {
           setError("The recovery code has expired. Please request a new one.");
         } else {
-          // Code is valid and not expired, proceed to the reset password page
-          navigate("/reset-password");  // Redirect to password reset page
+          
+          navigate("/reset-password");  
         }
       } else {
         setError("Invalid recovery code. Please try again.");
@@ -51,7 +51,7 @@ const PasswordRecovery = () => {
       <div className="reset-form">
         {/* Logo Image */}
         <h2 className="logo">
-          <img src={require("../assets/Screenshot 2023-08-19 at 15.11.22.png")} alt="Aktiv60 Logo" />
+          <img src={require("../assets/Aktiv60.png")} alt="Aktiv60 Logo" />
         </h2>
 
         <p className="reset-subtitle">
