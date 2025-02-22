09ccc476ed5510bb1697ed6a7e6513f875bc6c03
Blessiey <mogashoablessing676@gmail.com>
2025-02-22 13:36:12 +0200

    commit

--- src/pages/Location/LocationSettings.js
+++ src/pages/Location/LocationSettings.js
@@ -2,13 +2,9 @@ import React from 'react';
 
 const LocationSettings = () => {
     return (
-        <div className='settings-container'>
-            <h1 className='titlesettings'>Location Settings</h1>
-            <div className='offerings'>
-            <h2>Package offerings</h2>
-            <button className='package-btn'>Basic Package</button>
-            <button className='package-btn'>Premium Package</button>
-        </div>
+        <div>
+            <h1>Location Settings</h1>
+            <p>Configure your location settings here.</p>
         </div>
     );
 };
