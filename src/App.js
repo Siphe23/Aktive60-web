f7f7d8d2d626e581c6ebe26a5785c2d90ee59449
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2025-02-25 14:17:26 +0200

    updated login

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
@@ -5,10 +6,12 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+=======
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+>>>>>>> 419cd6d (updated login)
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { auth } from "./firebase"; // Ensure this is correctly imported
-import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -18,12 +21,21 @@ import TwoFactorAuth from "./components/TwoFactorAuth";
 import PasswordRecovery from "./components/PasswordRecovery";
 import ResetPassword from "./components/ResetPassword";
 import NotFound from "./components/NotFound";
+<<<<<<< HEAD
 import SuperProfile from "./components/SuperProfile";
 import Dashboard from "./pages/Dashboard";
 import StaffRegister from "./components/StaffRegister";
 import StaffProfile from "./components/StaffProfile";
 import StaffLogin from "./components/StaffLogin";
 import SuperLogin from "./components/superLogin";
+=======
+import SuperProfile from './components/SuperProfile';
+import Dashboard from './pages/Dashboard'
+import StaffRegister from './components/StaffRegister';
+import StaffProfile from './components/StaffProfile';
+import StaffLogin from './components/StaffLogin';
+import SuperLogin from './components/superLogin';
+>>>>>>> 419cd6d (updated login)
 import LocationOverview from "./pages/Location/LocationOverview/LocationOverview";
 import LocationDetails from "./pages/Location/LocationDetails/LocationDetails";
 import LocationStaff from "./pages/Location/LocationStaff/LocationStaff";
@@ -37,7 +49,12 @@ import Collection from "./pages/Collection";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
+
+
+
+
 function App() {
+<<<<<<< HEAD
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -52,6 +69,8 @@ function App() {
     return <div>Loading...</div>; // You can replace this with a loading spinner or component bafethu
   }
 
+=======
+>>>>>>> 419cd6d (updated login)
   return (
     <Router>
       <ToastContainer position="top-right" autoClose={3000} />
@@ -70,6 +89,7 @@ function App() {
         />
         <Route path="/two-factor-auth" element={<TwoFactorAuth />} />
         <Route path="/password-recovery" element={<PasswordRecovery />} />
+<<<<<<< HEAD
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/super-profile" element={<SuperProfile />} />
@@ -87,6 +107,29 @@ function App() {
         <Route path="/trainers" element={<Trainers />} />
         <Route path="/trainees" element={<Trainees />} />
         <Route path="/collection" element={<Collection />} />
+=======
+        <Route path=  "/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="SuperProfile" element={<SuperProfile />} />
+        <Route path="StaffRegister" element={<StaffRegister />} />
+        <Route path="StaffProfile" element={<StaffProfile />} />
+        <Route path="StaffLogin" element={<StaffLogin />} />
+        <Route path="SuperLogin" element={<SuperLogin />} />
+        <Route path="Navbar" element={<Navbar />} />
+           
+            <Route path="/locationoverview" element={<LocationOverview />} />
+            <Route path="/locationdetails" element={<LocationDetails />} />
+            <Route path="/locationstaff" element={<LocationStaff />} />
+            <Route path="/locationsettings" element={<LocationSettings />} />
+            <Route path="/settings" element={<Settings />} />
+            {/* <Route path="/branch-details" element={<BranchDetails />} /> */}
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/usermanagement" element={<UserManagement />} />
+            <Route path="/trainers" element={<Trainers />} />
+            <Route path="/trainees" element={<Trainees />} />
+            <Route path="/collection" element={<Collection />} />
+          
+>>>>>>> 419cd6d (updated login)
       </Routes>
     </Router>
   );
