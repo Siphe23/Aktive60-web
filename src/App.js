178c08b52ca7929c9324cf0ad9044da08f550e9d
Kronik502 <kronikwenaite@gmail.com>
2025-02-25 15:58:23 +0200

    Overall update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
@@ -6,10 +5,6 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-=======
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
->>>>>>> 419cd6d (updated login)
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
@@ -21,21 +16,12 @@ import TwoFactorAuth from "./components/TwoFactorAuth";
 import PasswordRecovery from "./components/PasswordRecovery";
 import ResetPassword from "./components/ResetPassword";
 import NotFound from "./components/NotFound";
-<<<<<<< HEAD
 import SuperProfile from "./components/SuperProfile";
 import Dashboard from "./pages/Dashboard";
 import StaffRegister from "./components/StaffRegister";
 import StaffProfile from "./components/StaffProfile";
 import StaffLogin from "./components/StaffLogin";
 import SuperLogin from "./components/superLogin";
-=======
-import SuperProfile from './components/SuperProfile';
-import Dashboard from './pages/Dashboard'
-import StaffRegister from './components/StaffRegister';
-import StaffProfile from './components/StaffProfile';
-import StaffLogin from './components/StaffLogin';
-import SuperLogin from './components/superLogin';
->>>>>>> 419cd6d (updated login)
 import LocationOverview from "./pages/Location/LocationOverview/LocationOverview";
 import LocationDetails from "./pages/Location/LocationDetails/LocationDetails";
 import LocationStaff from "./pages/Location/LocationStaff/LocationStaff";
@@ -54,7 +40,6 @@ import Sidebar from "./components/Sidebar";
 
 
 function App() {
-<<<<<<< HEAD
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -69,8 +54,6 @@ function App() {
     return <div>Loading...</div>; // You can replace this with a loading spinner or component bafethu
   }
 
-=======
->>>>>>> 419cd6d (updated login)
   return (
     <Router>
       <ToastContainer position="top-right" autoClose={3000} />
@@ -89,7 +72,6 @@ function App() {
         />
         <Route path="/two-factor-auth" element={<TwoFactorAuth />} />
         <Route path="/password-recovery" element={<PasswordRecovery />} />
-<<<<<<< HEAD
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/super-profile" element={<SuperProfile />} />
@@ -107,29 +89,6 @@ function App() {
         <Route path="/trainers" element={<Trainers />} />
         <Route path="/trainees" element={<Trainees />} />
         <Route path="/collection" element={<Collection />} />
-=======
-        <Route path=  "/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="SuperProfile" element={<SuperProfile />} />
-        <Route path="StaffRegister" element={<StaffRegister />} />
-        <Route path="StaffProfile" element={<StaffProfile />} />
-        <Route path="StaffLogin" element={<StaffLogin />} />
-        <Route path="SuperLogin" element={<SuperLogin />} />
-        <Route path="Navbar" element={<Navbar />} />
-           
-            <Route path="/locationoverview" element={<LocationOverview />} />
-            <Route path="/locationdetails" element={<LocationDetails />} />
-            <Route path="/locationstaff" element={<LocationStaff />} />
-            <Route path="/locationsettings" element={<LocationSettings />} />
-            <Route path="/settings" element={<Settings />} />
-            {/* <Route path="/branch-details" element={<BranchDetails />} /> */}
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/usermanagement" element={<UserManagement />} />
-            <Route path="/trainers" element={<Trainers />} />
-            <Route path="/trainees" element={<Trainees />} />
-            <Route path="/collection" element={<Collection />} />
-          
->>>>>>> 419cd6d (updated login)
       </Routes>
     </Router>
   );
